e016e87e5bfbd1bf6150a3973849ae57db61bef8
Oleh Velychko <ovelychko@binaryanvil.com>
2025-12-17 21:14:33 +0100

    Feat: Added slowapi to requirements, added limit 10request per second for route '/'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI()
 
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 @app.get("/")
-async def root():
+@limiter.limit("10/second")
+async def root(request: Request):
     return {"message": "Hello World"}
